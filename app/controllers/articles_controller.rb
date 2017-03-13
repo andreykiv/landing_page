239de867f35536eb6f3f91f8b7cc94068ead696f
Myroslav Andreykiv <andreykiv09@gmail.com>
2017-03-13 09:30:53 +0000

    hyperlinks added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,23 +4,25 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
     
-        
     def show
         @article = Article.find(params[:id])
     end
     
     def new
+        @article = Article.new
     end
     
-    def create
-        @article = Article.new(article_params)
-        
-        @article.save
+   def create
+    @article = Article.new(article_params)
+ 
+    if @article.save
         redirect_to @article
+    else
+        render 'new'
     end
-    
-    
-    private
+    end
+
+ private
     def article_params
         params.require(:article).permit(:title, :text)
     end
